06e4d7e7589d44af1950d61f50c3f814fb097d14
geov94 <agsalamanca@uao.edu.co>
2022-05-05 20:30:32 -0500

    se aplican cambios de la clase 8 : template

---
+++ template/my-element.js
@@ -0,0 +1,55 @@
+class myElement extends HTMLElement {
+    constructor (){
+        super();        
+       
+    }
+
+    getTemplate(){
+      const template = document.createElement('template')
+
+      template.innerHTML = `
+      
+       <section>
+         <h2>estudiando el curso de web components</h2>
+         <div>
+          <p>soy otro texto de ejemplo</p>
+        </div>      
+       </section> 
+       ${this.getStyles()}
+            
+           
+           
+      `;
+
+      return template;
+
+
+
+    }
+
+    getStyles(){
+      return `
+        <style>
+          h2{
+            color:red;
+          }
+        </style> 
+
+
+      `;
+    }
+
+    render(){
+      this.appendChild(this.getTemplate().content.cloneNode(true))
+
+    }
+
+    connectedCallback() {
+
+       this.render() 
+    }
+
+
+}
+
+customElements.define('my-element',myElement);
