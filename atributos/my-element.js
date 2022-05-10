c9ddb5fa2b39f3d94fd474a806aa938c6ad592fb
geov94 <agsalamanca@uao.edu.co>
2022-05-10 16:25:49 -0500

    se agrega la carpeta atributos y se hace el ejemplo de la clase con zelda: a link to the past

---
+++ atributos/my-element.js
@@ -0,0 +1,70 @@
+class myElement extends HTMLElement {
+
+    constructor (){
+      super();   
+
+      this.attachShadow({mode :"open"})  
+      this.title = this.getAttribute("title")
+      this.paragraph = this.getAttribute("paragraph")
+      this.img = this.getAttribute("img")
+            
+       
+    }
+
+    getStyles(){
+      return `
+        <style>
+          h2{
+            color:red;
+          }
+        </style> 
+       
+
+      `;
+    }
+
+    getTemplate(){
+      const template = document.createElement("template")
+
+      template.innerHTML = `
+      
+       <section>
+         <h2>
+           ${this.title}
+         </h2> 
+         <div>
+           <p>  
+             ${this.paragraph}       
+           </p>
+           <img src=${this.img} width="420" height="300"> 
+         </div>
+
+       </section> 
+       ${this.getStyles()}
+            
+           
+           
+      `;
+
+      return template;
+
+
+
+    }
+
+    
+
+    render(){
+      this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true))
+
+    }
+
+    connectedCallback() {
+
+       this.render() 
+    }
+
+
+}
+
+customElements.define('my-element',myElement);
