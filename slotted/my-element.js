89a543142663e1f3876f1175d3af8c021a5d49b8
Geov94 <agsalamanca@misena.edu.co>
2022-05-15 12:23:28 -0500

    se agrega slotted

---
+++ slotted/my-element.js
@@ -0,0 +1,69 @@
+class myElement extends HTMLElement {
+
+    constructor(){
+
+        super();
+        this.attachShadow({mode:'open'})
+    }
+
+    getStyles(){
+        return `
+
+         <style>
+             ::slotted(span){
+                 font-size:30px;
+                 color:red;
+
+             }    
+             ::slotted(.text){
+                 
+                 color:blue;
+
+              }         
+         </style>     
+                 
+         
+        `;
+        
+    }
+
+    getTemplate(){
+        const template = document.createElement("template");
+
+        template.innerHTML = `
+
+        <section>
+         <h1>
+           <slot name="title"></slot>
+         </h1>
+         <h2>
+           <slot></slot>
+         </h2> 
+         <div>
+          <p>
+            <slot name="paragraph"></slot>
+          </p>
+         </div>
+       </section>  
+       ${this.getStyles()}
+
+               
+               
+                
+        `;
+        return template;
+    }
+
+    render(){
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true))
+    }
+
+    connectedCallback(){
+        this.render();
+    }
+
+
+
+}
+
+customElements.define('my-element',myElement)
